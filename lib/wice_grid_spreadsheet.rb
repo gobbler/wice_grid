4d795e200b142a41982e076fe65f227ebd7e8d62
Aleksandr Furmanov <afurmanov@gobbler.com>
2012-04-24 15:43:11 -0700

    Ruby 1.9 code only for generating CSV reports

--- lib/wice_grid_spreadsheet.rb
+++ lib/wice_grid_spreadsheet.rb
@@ -5,8 +5,6 @@ module Wice
   class Spreadsheet  #:nodoc:
 
     attr_reader :tempfile
-    # CSV in 1.9.1 is a version of FasterCSV
-    if RUBY_VERSION == '1.9.1' || RUBY_VERSION == '1.9.2'
 
       def initialize(name, field_separator)  #:nodoc:
         @tempfile = Tempfile.new(name)
@@ -17,17 +15,5 @@ module Wice
         @csv << row
       end
       
-    else
-      def initialize(name, field_separator)  #:nodoc:
-        @tempfile = Tempfile.new(name)
-        @field_separator = field_separator
-      end
-
-      def << (row)  #:nodoc:
-        CSV::Writer.generate(@tempfile, @field_separator) do |csv|
-           csv << row.map(&:to_s)
-        end
-      end
-    end
   end
 end
